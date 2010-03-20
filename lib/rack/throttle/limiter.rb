e513109a70a72f5a7fb22f9d4bc076b341fd1622
Arto Bendiken <arto@bendiken.net>
2010-03-20 22:59:25 +0100

    Implemented Rack::Throttle::Limiter#forbidden.

--- lib/rack/throttle/limiter.rb
+++ lib/rack/throttle/limiter.rb
@@ -1,6 +1,8 @@
 module Rack module Throttle
   ##
   class Limiter
+    attr_reader :app
+
     ##
     # @param  [#call]                  app
     # @param  [Hash{Symbol => Object}] options
@@ -12,7 +14,36 @@ module Rack module Throttle
     # @param  [Hash{String => String}] env
     # @return [Array(Integer, Hash, #each)]
     def call(env)
-      # TODO
+      app.call(env) # TODO
+    end
+
+    ##
+    # Outputs an HTTP `403 Forbidden` response.
+    #
+    # @param  [String, #to_s] message
+    # @return [Array(Integer, Hash, #each)]
+    def forbidden(message = nil)
+      http_error(403, message)
+    end
+
+    ##
+    # Outputs an HTTP 4xx or 5xx response.
+    #
+    # @param  [Integer]       code
+    # @param  [String, #to_s] message
+    # @return [Array(Integer, Hash, #each)]
+    def http_error(code, message = nil)
+      [code, {'Content-Type' => 'text/plain; charset=utf-8'},
+        http_status(code) + (message.nil? ? "\n" : " (#{message})\n")]
+    end
+
+    ##
+    # Returns the HTTP status message for the given status `code`.
+    #
+    # @param  [Integer] code
+    # @return [String]
+    def http_status(code)
+      [code, Rack::Utils::HTTP_STATUS_CODES[code]].join(' ')
     end
   end
 end end
