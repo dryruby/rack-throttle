27927882d56126a8d1339c49f268628820d1b9a3
Arto Bendiken <arto@bendiken.net>
2010-03-21 05:44:33 +0100

    Added a note about the daily/hourly rate limit strategies not being implemented yet.

--- lib/rack/throttle/daily.rb
+++ lib/rack/throttle/daily.rb
@@ -4,6 +4,8 @@ module Rack; module Throttle
   # maximum number of allowed HTTP requests per day (by default, 86,400
   # requests per 24 hours, which works out to an average of 1 request per
   # second).
+  #
+  # _Not yet implemented in the current release._
   class Daily < Limiter
     # TODO
   end
--- lib/rack/throttle/hourly.rb
+++ lib/rack/throttle/hourly.rb
@@ -4,6 +4,8 @@ module Rack; module Throttle
   # maximum number of allowed HTTP requests per hour (by default, 3,600
   # requests per 60 minutes, which works out to an average of 1 request per
   # second).
+  #
+  # _Not yet implemented in the current release._
   class Hourly < Limiter
     # TODO
   end
