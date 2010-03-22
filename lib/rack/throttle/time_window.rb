13f44f92e8f2bad70fc11fcfae3472998d6e9733
Arto Bendiken <arto@bendiken.net>
2010-03-23 00:56:15 +0100

    Ensured that TimeWindow inputs are correctly duck-typed.

--- lib/rack/throttle/time_window.rb
+++ lib/rack/throttle/time_window.rb
@@ -9,7 +9,7 @@ module Rack; module Throttle
     # @return [Boolean]
     def allowed?(request)
       count = cache_get(key = cache_key(request)).to_i + 1 rescue 1
-      allowed = count <= max_per_window
+      allowed = count <= max_per_window.to_i
       begin
         cache_set(key, count)
         allowed
