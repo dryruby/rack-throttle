b101a5ba2ca25774a8ed46ecce3999e78cba4ebf
Brendon Murphy <xternal1+github@gmail.com>
2010-03-22 01:15:57 -0700

    Fixed some counting logic in TimeWindow

--- lib/rack/throttle/time_window.rb
+++ lib/rack/throttle/time_window.rb
@@ -1,10 +1,10 @@
 module Rack; module Throttle
   class TimeWindow < Limiter
     def allowed?(request)
-      count = cache_get(key = cache_key(request)).to_i rescue nil
-      allowed = !count || (count + 1) < max_per_window
+      count = cache_get(key = cache_key(request)).to_i + 1 rescue 1
+      allowed = count <= max_per_window
       begin
-        cache_set(key, count + 1)
+        cache_set(key, count)
         allowed
       rescue => e
         allowed = true
