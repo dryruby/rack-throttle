535c3994db386de2fff43b0c6f6fed9d14f70f21
TJ Singleton <tjsingleton@vantagestreet.com>
2011-03-07 16:55:02 -0500

    return an array of strings for 1.9
    http://rack.rubyforge.org/doc/files/SPEC.html

--- lib/rack/throttle/limiter.rb
+++ lib/rack/throttle/limiter.rb
@@ -188,7 +188,7 @@ module Rack; module Throttle
     # @return [Array(Integer, Hash, #each)]
     def http_error(code, message = nil, headers = {})
       [code, {'Content-Type' => 'text/plain; charset=utf-8'}.merge(headers),
-        http_status(code) + (message.nil? ? "\n" : " (#{message})\n")]
+        [http_status(code), (message.nil? ? "\n" : " (#{message})\n")]]
     end
 
     ##
