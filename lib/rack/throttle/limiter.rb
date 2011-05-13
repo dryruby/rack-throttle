d1a65f1e6883b86faf289c1babbea1073c9e0b20
Winfield Peterson <wpeterson@patientslikeme.com>
2011-05-13 14:13:16 -0400

    Add an optional callback to the Limiter base strategy.  This callback is fired whenever a rate limit is exceeded, to allow something like logging or per-app handling of rate limits.

--- lib/rack/throttle/limiter.rb
+++ lib/rack/throttle/limiter.rb
@@ -31,7 +31,7 @@ module Rack; module Throttle
     # @see    http://rack.rubyforge.org/doc/SPEC.html
     def call(env)
       request = Rack::Request.new(env)
-      allowed?(request) ? app.call(env) : rate_limit_exceeded
+      allowed?(request) ? app.call(env) : rate_limit_exceeded(request)
     end
 
     ##
@@ -174,7 +174,8 @@ module Rack; module Throttle
     # Outputs a `Rate Limit Exceeded` error.
     #
     # @return [Array(Integer, Hash, #each)]
-    def rate_limit_exceeded
+    def rate_limit_exceeded(request)
+      options[:rate_limit_exceeded_callback].call(request) if options[:rate_limit_exceeded_callback]
       headers = respond_to?(:retry_after) ? {'Retry-After' => retry_after.to_f.ceil.to_s} : {}
       http_error(options[:code] || 403, options[:message] || 'Rate Limit Exceeded', headers)
     end
--- spec/limiter_spec.rb
+++ spec/limiter_spec.rb
@@ -2,49 +2,65 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe Rack::Throttle::Limiter do
   include Rack::Test::Methods
-  
-  def app
-    @target_app ||= example_target_app
-    @app ||= Rack::Throttle::Limiter.new(@target_app)
+
+  describe 'with default config' do
+    def app
+      @target_app ||= example_target_app
+      @app ||= Rack::Throttle::Limiter.new(@target_app)
+    end
+
+    describe "basic calling" do
+      it "should return the example app" do
+        get "/foo"
+        last_response.body.should show_allowed_response
+      end
+
+      it "should call the application if allowed" do
+        app.should_receive(:allowed?).and_return(true)
+        get "/foo"
+        last_response.body.should show_allowed_response
+      end
+
+      it "should give a rate limit exceeded message if not allowed" do
+        app.should_receive(:allowed?).and_return(false)
+        get "/foo"
+        last_response.body.should show_throttled_response
+      end
+    end
+
+    describe "allowed?" do
+      it "should return true if whitelisted" do
+        app.should_receive(:whitelisted?).and_return(true)
+        get "/foo"
+        last_response.body.should show_allowed_response
+      end
+
+      it "should return false if blacklisted" do
+        app.should_receive(:blacklisted?).and_return(true)
+        get "/foo"
+        last_response.body.should show_throttled_response
+      end
+
+      it "should return true if not whitelisted or blacklisted" do
+        app.should_receive(:whitelisted?).and_return(false)
+        app.should_receive(:blacklisted?).and_return(false)
+        get "/foo"
+        last_response.body.should show_allowed_response
+      end
+    end
   end
 
-  describe "basic calling" do
-    it "should return the example app" do
-      get "/foo"
-      last_response.body.should show_allowed_response
+  describe 'with rate_limit_exceeded callback' do
+    def app
+      @target_app ||= example_target_app
+      @app ||= Rack::Throttle::Limiter.new(@target_app, :rate_limit_exceeded_callback => lambda {|request| @app.callback(request) } )
     end
-  
-    it "should call the application if allowed" do
-      app.should_receive(:allowed?).and_return(true)
-      get "/foo"
-      last_response.body.should show_allowed_response
-    end
-  
-    it "should give a rate limit exceeded message if not allowed" do
-      app.should_receive(:allowed?).and_return(false)
-      get "/foo"
-      last_response.body.should show_throttled_response
-    end
-  end
-  
-  describe "allowed?" do
-    it "should return true if whitelisted" do
-      app.should_receive(:whitelisted?).and_return(true)
-      get "/foo"
-      last_response.body.should show_allowed_response
-    end
-    
-    it "should return false if blacklisted" do
+
+    it "should call rate_limit_exceeded_callback w/ request when rate limit exceeded" do
       app.should_receive(:blacklisted?).and_return(true)
+      app.should_receive(:callback).and_return(true)
       get "/foo"
       last_response.body.should show_throttled_response
-    end
-    
-    it "should return true if not whitelisted or blacklisted" do
-      app.should_receive(:whitelisted?).and_return(false)
-      app.should_receive(:blacklisted?).and_return(false)
-      get "/foo"
-      last_response.body.should show_allowed_response
     end
   end
 end
