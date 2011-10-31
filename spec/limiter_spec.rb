3d347833d0d3e5f4de93d673cd0fbb3885a3ea45
Matt Griffin <matt@griffinonline.org>
2011-10-31 11:13:17 -0400

    Fix spec require paths

--- spec/limiter_spec.rb
+++ spec/limiter_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require 'spec_helper'
 
 describe Rack::Throttle::Limiter do
   include Rack::Test::Methods
--- spec/minute_spec.rb
+++ spec/minute_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require 'spec_helper'
 
 describe Rack::Throttle::Hourly do
   include Rack::Test::Methods
